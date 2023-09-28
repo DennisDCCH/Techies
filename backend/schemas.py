1688fdb6c570fae2e745a1235b6ad361f8fbfb69
Brandon Jang <bjang002@e.ntu.edu.sg>
2023-09-28 21:47:33 +0800

    Added Schemas and skeleton endpoints

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,5 +1,30 @@
 from marshmallow import Schema, fields
+
 class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     password = fields.Str(required=True, load_only=True)
+    profile_picture = fields.Str(required=True)
+    gender = fields.Str(required=True)
+
+class CoachingServiceSchema(Schema):
+    id = fields.Int(dump_only=True)
+    coach_id = fields.Int(required=True)
+    sport = fields.Str(required=True)
+    location = fields.Str(required=True)
+    price = fields.Float(required=True)
+    availability = fields.Str(required=True)
+    description = fields.Str(required=True)
+
+#add in coachingserviceUpdateschema
+
+class ReviewSchema(Schema):
+    id = fields.Int(dump_only=True)
+    service_id = fields.Int(required=True)
+    user_id = fields.Int(required=True)
+    rating = fields.Int(required=True)
+    comment = fields.Str(required=True)
+
+
+
+ 
