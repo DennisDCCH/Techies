2465794ab86ba2466c33e870e14a71506e3d8c82
Dennis Chen <dennischencheehaodcch@gmail.com>
2023-10-06 01:47:42 +0800

    add new select button to card3 component
    to do list
    1. edit profile page
    2. edit listing page
    3. marketplace page add input box / filter
    4. map page
    5. chat page

--- frontend/src/components/card3.jsx
+++ frontend/src/components/card3.jsx
@@ -1,11 +1,17 @@
 import React from "react"
+import { Link } from "react-router-dom"
+
 import "./card3.css"
+
 
 
 export default function Card3(props) {
     return (
         <div className = "card3">
             <h1 className = "card3-title">{props.item.name}</h1>
+            <button className = "card3-select-button">
+                <Link className = "card3-select-link">Select</Link>
+            </button>
             <button className = "card3-delete-button">Delete</button>
         </div>
     )
