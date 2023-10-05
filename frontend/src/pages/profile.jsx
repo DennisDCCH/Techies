e16522f6903f7422bf8cb4ce6bf6bd1fa36bb28b
Dennis Chen <dennischencheehaodcch@gmail.com>
2023-10-05 11:12:26 +0800

    changed styling for profile

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -2,10 +2,11 @@ import React from "react"
 import data from "../data/user.js"
 import Sidebar from "../components/sidebarmenu.jsx"
 import AccountInfo from "../components/accountinfo.jsx"
+import "./profile.css"
 
 export default function Profile() {
     return (
-        <div>
+        <div className = "profile-container">
             <Sidebar 
                 id = {data.id}
                 item = {data}
