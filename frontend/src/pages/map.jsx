78294807b9fe18f335a86c69bf3e6b3b0628baf3
Chai Jie En <ch0039en@e.ntu.edu.sg>
2023-10-08 00:28:41 +0800

    Revert "Merge branch 'main' of https://github.com/DennisDCCH/Techies"
    This reverts commit c4e380a545559f847856c48309caf97af2e6e59a, reversing
    changes made to 27977a70dc935ce337fd66093be7e7603ed0143e.

--- frontend/src/pages/map.jsx
+++ frontend/src/pages/map.jsx
@@ -1,28 +1,7 @@
 import React from "react"
-import { GoogleMap, useLoadScript } from "@react-google-maps/api"
-import "./map.css"
-
-const center = {
-    lat: 1.342792,
-    lng: 103.682346}
 
 export default function Map() {
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: "AIzaSyAqfV5D6spu0saYX6khc2BQJsoSsK8vAVA",
-    })
-
-    if (!isLoaded) return <div>Loading...</div>
-
     return (
-        <div>
-            <h1>Loaded</h1>
-            <GoogleMap
-                zoom = {20}
-                center = {center}
-                mapContainerClassName="map-container"
-            >
-
-            </GoogleMap>
-        </div>
+        <h1>Map page</h1>
     )
 }
