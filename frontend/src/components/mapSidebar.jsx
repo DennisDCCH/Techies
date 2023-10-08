5b22214bad1074a10b14ebcd761eb6b66fbfa674
Dennis Chen <dennischencheehaodcch@gmail.com>
2023-10-08 23:15:15 +0800

    fixing 2.0

---
+++ frontend/src/components/mapSidebar.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import MAGNIFYINGLOGO from "../images/magnifying-logo.png"
+
+import "./mapSidebar.css"
+
+export default function MapSidebar (props) {
+    return (
+        <div className = "mapsidebar-background">
+            <div className = "mapsidebar-user-info"> 
+                <img className = "mapsidebar-user-info-pic" src= {`images/${props.item.userImg}`} alt="Logo" />
+                <h1 className = "mapsidebar-user-info-username">{props.item.username}</h1>
+            </div>
+            <div className = "mapsidebar-listing">
+                <Link className = "mapsidebar-home" to = "/homepage">Home</Link>
+            </div>
+            <div className = "mapsidebar-input">
+                <input className = "mapsidebar-input-box" type="text" placeholder = "Input Location"/>
+                <input className = "mapsidebar-input-button"type = "image" src = {MAGNIFYINGLOGO}/>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/map.jsx
+++ frontend/src/pages/map.jsx
@@ -1,7 +1,34 @@
-import React from "react"
+import React, { useMemo } from "react"
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
+
+import data from "../data/user"
+
+import MapSidebar from "../components/mapSidebar"
+import "./map.css"
+
+const center = {lat: 1.3521,lng: 103.8198}
 
 export default function Map() {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: "AIzaSyAqfV5D6spu0saYX6khc2BQJsoSsK8vAVA",
+    })
+
+    if (!isLoaded) return <div>Loading...</div>
+
     return (
-        <h1>Map page</h1>
+        <div className = "map-page-container">
+            <MapSidebar
+                item = {data}
+            />
+            <div className = "map-container">
+                <GoogleMap
+                    zoom = {15}
+                    center = {center}
+                    mapContainerClassName="map-size"
+                >
+                    <Marker position = {center}/>
+                </GoogleMap>
+            </div>
+        </div>
     )
 }
