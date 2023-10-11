7be7163a3843c64bc01a38b1511832a5b3048bac
Dennis Chen <126561730+DennisDCCH@users.noreply.github.com>
2023-10-11 16:03:15 +0800

    test

--- frontend/src/actions/registerAction.js
+++ frontend/src/actions/registerAction.js
@@ -1,4 +1,7 @@
 import { redirect } from "react-router-dom"
+import axios from "../api/axios"
+
+const LOGIN_URL = '/register'
 
 export const registerAction = async ({ request }) => {
     const data = await request.formData()
@@ -33,7 +36,37 @@ export const registerAction = async ({ request }) => {
         return {error: "Password must have at least 1 special character"};
 
 
-    console.log(submission);
+    console.log(submission); 
+
+    try {
+        const response = await axios.post(LOGIN_URL, // attaches itself to the baseUrl that we alr defined in the api directory
+            JSON.stringify({ submission }),           // sends the user's credentials (user and pwd) to the login URL
+            {
+                headers: { 'Content-Type:': 'application/json '},
+                withCredentials: true
+                // ensures that any cookies set by the server during this request are saved 
+                // and sent with subsequent requests, which is crucial for session-based authentication or when using HTTP-only cookies.
+            }
+        );
+        console.log(JSON.stringify(response?.data));  // store token in memory
+        const accessToken = response?.data?.accessToken;
+        const roles = response?.data?.roles;
+        setAuth({ submission });  // update local state
+        navigate(from, { replace: true });
+    } catch (err) {
+
+        return {err: "Password must have at least 1 special character"} 
+        // if (!err?.response) {
+        //     setErrMsg('NoServerResponse');
+        // } else if (err.response?.status === 400) {
+        //     setErrMsg('Missing Username or Password');
+        // } else if (err.response?.status === 401) {
+        //     setErrMsg('Unauthorized');
+        // } else {
+        //     setErrMsg('Login Failed');
+        // }
+        // errRef.current.focus();
+    }
 
     return redirect("/")
 }
