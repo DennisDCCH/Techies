da46ee3015b7e3ee687b6724b8e0c4881f85f405
ieatsighpies <76864712+ieatsighpies@users.noreply.github.com>
2023-10-16 11:49:42 +0800

    modified:   frontend/src/pages/map.jsx

--- frontend/src/pages/map.jsx
+++ frontend/src/pages/map.jsx
@@ -1,7 +1,7 @@
-import React, { useMemo } from "react"
+import React, { useState, useEffect } from "react"
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
 
-import data from "../data/user"
+import userData from "../data/user"
 
 import MapSidebar from "../components/mapSidebar"
 import "./map.css"
@@ -13,12 +13,28 @@ export default function Map() {
         googleMapsApiKey: "AIzaSyAqfV5D6spu0saYX6khc2BQJsoSsK8vAVA",
     })
 
+    const [apiData, setApiData] = useState([]);
+
+    useEffect(() => {
+      // Fetch data from your API
+      const fetchData = async () => {
+        try {
+          const response = await fetch("https://api.data.gov.sg/v1/transport/taxi-availability");
+          const data = await response.json();
+          setApiData(data);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      };
+      fetchData();
+    }, []);
+
     if (!isLoaded) return <div>Loading...</div>
 
     return (
         <div className = "map-page-container">
             <MapSidebar
-                item = {data}
+                item = {userData}
             />
             <div className = "map-container">
                 <GoogleMap
@@ -26,7 +42,12 @@ export default function Map() {
                     center = {center}
                     mapContainerClassName="map-size"
                 >
-                    <Marker position = {center}/>
+                    {apiData.map((item, index) => (
+                        <Marker
+                        key={index}
+                        position={{ lat: item.latitude, lng: item.longitude }}
+                        />
+                    ))}
                 </GoogleMap>
             </div>
         </div>
