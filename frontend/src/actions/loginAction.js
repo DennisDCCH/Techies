cd98a0ee28ff680ae5bba40a210c96cf082d1334
Dennis Chen <126561730+DennisDCCH@users.noreply.github.com>
2023-10-18 13:08:05 +0800

    test

--- frontend/src/actions/loginAction.js
+++ frontend/src/actions/loginAction.js
@@ -17,26 +17,16 @@ export const loginAction = async ({ request }) => {
 
         if (response.status === 200) {
 
-            if (response.headers['set-cookie']) {
-                console.log('A Set-Cookie header is present in the response:', response.headers['set-cookie']);
-              } else {
-                console.log('No Set-Cookie header in the response.');
-              }
             console.log(response.data.access_token)
-
             return redirect("/homepage");
-        } else if (response.status === 409) {
-
-            const errorResponse = await response.json();
-            const errorMessage = errorResponse.message;
-            return { error: errorMessage };
         } else {
             // Handle other response statuses as needed
             return { error: "Login failed" };
         }
     } catch (error) {
         // Handle any Axios request error, such as network issues or server unavailability
-        console.log(error)
-        return { error: "An error occurred while registering" };
+        console.log(error.response.data.message)
+        const errorMessage = error.response.data.message
+        return { error: errorMessage };
     }
 }
--- frontend/src/components/card3.jsx
+++ frontend/src/components/card3.jsx
@@ -1,18 +1,34 @@
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 import "./card3.css"
-
-
+import axios from "../api/axios";
 
 export default function Card3(props) {
+    const location = useLocation();
+    const currentURL = location.pathname;
+
+    const handleDelete = async () => {
+        try {
+            //Choosing which endpoint
+            const deleteURL = currentURL === '/bookedlisting' ? `/booking/${props.item.id}` : `/save/${props.item.id}`;
+        
+            console.log(deleteURL)
+            // Send the DELETE request
+            await axios.delete(deleteURL);
+            console.log(`Item with id ${props.item.id} deleted successfully`);
+        } catch (error) {
+            console.error('Error deleting item:', error);
+        }
+      }
+
     return (
         <div className = "card3">
             <h1 className = "card3-title">{props.item.name}</h1>
             <button className = "card3-select-button">
-                <Link className = "card3-select-link">Select</Link>
+                <Link className = "card3-select-link" to = {`/listing/${props.item.id}`}>Select</Link>
             </button>
-            <button className = "card3-delete-button">Delete</button>
+            <button className = "card3-delete-button" onClick={handleDelete}>Delete</button>
         </div>
     )
 }
